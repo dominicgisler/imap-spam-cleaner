ee8213774e5a68e6cdfc08aabe4878521cdf8f2f
Dominic Gisler <domgisler@gmail.com>
2026-01-25 15:34:33 +0100

    enhanced config validation for provider/whitelist

--- config/config.go
+++ config/config.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"fmt"
 	"os"
 	"regexp"
+	"time"
 
 	"github.com/dominicgisler/imap-spam-cleaner/logx"
 	"github.com/go-playground/validator/v10"
@@ -28,19 +30,19 @@ type Provider struct {
 }
 
 type Inbox struct {
-	Schedule  string `yaml:"schedule"  validate:"required"`
-	Host      string `yaml:"host"      validate:"required"`
-	Port      int    `yaml:"port"      validate:"required"`
-	TLS       bool   `yaml:"tls"       validate:"omitempty"`
-	Username  string `yaml:"username"  validate:"required"`
-	Password  string `yaml:"password"  validate:"required"`
-	Provider  string `yaml:"provider"  validate:"required"`
-	Inbox     string `yaml:"inbox"     validate:"required"`
-	Spam      string `yaml:"spam"      validate:"required"`
-	MinScore  int    `yaml:"minscore"  validate:"required"`
-	MinAge    string `yaml:"minage"    validate:"omitempty"`
-	MaxAge    string `yaml:"maxage"    validate:"omitempty"`
-	Whitelist string `yaml:"whitelist" validate:"omitempty"`
+	Schedule  string        `yaml:"schedule"  validate:"required"`
+	Host      string        `yaml:"host"      validate:"required"`
+	Port      int           `yaml:"port"      validate:"required"`
+	TLS       bool          `yaml:"tls"       validate:"omitempty"`
+	Username  string        `yaml:"username"  validate:"required"`
+	Password  string        `yaml:"password"  validate:"required"`
+	Provider  string        `yaml:"provider"  validate:"required"`
+	Inbox     string        `yaml:"inbox"     validate:"required"`
+	Spam      string        `yaml:"spam"      validate:"required"`
+	MinScore  int           `yaml:"minscore"  validate:"required,gte=0,lte=100"`
+	MinAge    time.Duration `yaml:"minage"    validate:"omitempty"`
+	MaxAge    time.Duration `yaml:"maxage"    validate:"omitempty"`
+	Whitelist string        `yaml:"whitelist" validate:"omitempty"`
 }
 
 func Load() (*Config, error) {
@@ -61,6 +63,15 @@ func Load() (*Config, error) {
 
 	if config.Logging.Level != "" {
 		logx.SetLevel(config.Logging.Level)
+	}
+
+	for i, inbox := range config.Inboxes {
+		if _, ok := config.Providers[inbox.Provider]; !ok {
+			return nil, fmt.Errorf("invalid provider %s for inbox #%d", inbox.Provider, i)
+		}
+		if _, ok := config.Whitelists[inbox.Whitelist]; !ok {
+			return nil, fmt.Errorf("invalid whitelist %s for inbox #%d", inbox.Whitelist, i)
+		}
 	}
 
 	logx.Debug("Loaded config")
