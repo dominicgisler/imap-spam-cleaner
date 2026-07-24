7ac2daf7aaa69d00c8fe57007169413750fca740
Dominic Gisler <domgisler@gmail.com>
2026-07-24 22:49:46 +0200

    add rspamd provider

--- app/config.go
+++ app/config.go
@@ -25,7 +25,7 @@ type Logging struct {
 }
 
 type Provider struct {
-	Type   string            `yaml:"type"   validate:"required,oneof=openai ollama spamassassin gemini"`
+	Type   string            `yaml:"type"   validate:"required,oneof=openai ollama spamassassin gemini rspamd"`
 	Config map[string]string `yaml:"config" validate:"required"`
 }
 
